7dfc53ace3ef1b943e5dbfcd1746121192b23b1b
reynaldo victor <iscreynaldovictor@gmail.com>
2016-07-29 11:00:14 -0500

    agrega el formulario alta alumno

--- resources/views/alumnos/segmentos/campos.blade.php
+++ resources/views/alumnos/segmentos/campos.blade.php
@@ -1,3 +1,4 @@
 <div class="form-group">
-
+    {!! Form::label('lblcurp','CURP',array('class'=>'sr-only')) !!}
+    {!! Form::text('curp',null,array('class'=>'form-control','id'=>'lblcurp','placeholder'=>'Introduce tu CURP')) !!}
 </div>
